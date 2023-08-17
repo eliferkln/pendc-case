506516119bc7a55ec91568a3f050a3c7538e3914
Hanife Erkalan <73136159+eliferkln@users.noreply.github.com>
2023-08-18 00:55:11 +0300

    feat: graphic top menu created

--- src/components/Graphic/Graphic.jsx
+++ src/components/Graphic/Graphic.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./Graphic.css";
+import arrow from "../../assets/navbar/arrow down.png"
 
 const Graphic = () => {
   return (
-    <div>Graphic</div>
+<div className="graphic">
+    <div className="graphic-header">
+    <div className="graphic-section">
+            <p>Port Kullanım Bilgisi</p>
+            <img alt='' src={arrow}/>
+        </div>
+        <div className="graphic-list">
+        <ul>
+            <li>Canlı</li>
+            <li>2 Saat</li>
+            <li>48 Saat</li>
+            <li>1 Ay</li>
+            <li>1 Yıl</li>
+        </ul>
+        </div>
+    </div>
+    <div className="graphic-container"></div>
+</div>
   )
 }
 
