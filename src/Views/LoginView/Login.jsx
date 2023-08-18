cef0d895ff1a1aaa4ace9bf09ae0494485758116
Hanife Erkalan <73136159+eliferkln@users.noreply.github.com>
2023-08-18 14:43:00 +0300

    feat: login err notification

--- src/Views/LoginView/Login.jsx
+++ src/Views/LoginView/Login.jsx
@@ -1,48 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./Login.css";
-import Logo from "../../assets/Logo.png"
-import LoginPng from "../../assets/login.png"
+import Logo from "../../assets/Logo.png";
+import LoginPng from "../../assets/login.png";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = ({setShow}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const handleLogin = () => {
+    if (email === "admin@gmail.com" && password === "123") {
+      setShow(true);
+    } else {
+        setShow(false)
+        toast.error('Error !', {
+            position: toast.POSITION.TOP_CENTER
+        });
+    }
+  };
+
   return (
-
-<div className="container">
-    <div className="wrapper">
+    <div className="container">
+        <ToastContainer />
+      <div className="wrapper">
         <div className="left">
-   
-            <div className="login-form">
-               <div className="logo">
-                <img src={Logo} alt=''/>
-                </div>
-                <div className="login">
-                <h1>Pendc’ye Hoşgeldiniz</h1>
-                    <p>
-                   Kullanıcı bilgileriniz ile giriş yapabilirsiniz.
-                    </p>
-                    <div className="mail">
-                             <span>E-Posta Adresi</span>
-                             <input placeholder='isminiz@mail.com'/>
-                        </div>
-                         <div className="pass">
-                             <span >Şifreniz</span>
-                             <input placeholder='Şifrenizi Giriniz'/>
-                        </div>
-                         <button onClick={()=>setShow(true)}>Giriş Yap</button>
-                         <div className="login-footer">
-                        <h5>Bilgilerinizi hatırlamıyor musunuz?
- </h5> 
- <h6>Şifremi Unuttum
-     </h6>
-     </div>
-                </div>
+          <div className="login-form">
+            <div className="logo">
+              <img src={Logo} alt=''/>
             </div>
+            <div className="login">
+              <h1>Pendc’ye Hoşgeldiniz</h1>
+              <p>
+                Kullanıcı bilgileriniz ile giriş yapabilirsiniz.
+              </p>
+              <div className="mail">
+                <span>E-Posta Adresi</span>
+                <input placeholder='isminiz@mail.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+              </div>
+              <div className="pass">
+                <span>Şifreniz</span>
+                <input type="password" placeholder='Şifrenizi Giriniz' value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <button onClick={handleLogin}>Giriş Yap</button>
+              <div className="login-footer">
+                <h5>Bilgilerinizi hatırlamıyor musunuz?</h5>
+                <h6>Şifremi Unuttum</h6>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="right" ><div className="bg" style={{backgroundImage:`url(${LoginPng})`}}></div></div>
+        <div className="right">
+          <div className="bg" style={{backgroundImage:`url(${LoginPng})`}}></div>
+        </div>
+      </div>
     </div>
-    
- </div>
-
   )
 }
 
-export default Login
+export default Login;
